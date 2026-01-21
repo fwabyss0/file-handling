61fb2ede81c07035390ba37747fc4ce417cb373b
Alish <alishshrestha444@gmail.com>
2026-01-21 14:29:45 +0545

    update

--- p1.py
+++ p1.py
@@ -29,7 +29,28 @@ print("File copied successfully.")'''
             word_count = len(words)  
             print(f"Line {line_number}: {word_count} words")
             line_number += 1  
-count_line("sample.txt")     '''      
+count_line("sample.txt")     '''   
+   
+#q.4
+'''def count_line(f):
+    with open(f, "r") as file:
+        line_counts=0
+        for line in file:
+            line_counts += 1
+            print("Total number of lines in story:", line_counts)
+count_line("sample.txt")'''
+
+#q.5
+'''def copy_python_lines(f):
+    with open(f, "r") as source, open("management.txt", "w") as destination:
+        for line in source:
+            if "Python" in line:
+                destination.write(line)
+copy_python_lines("employee.txt")
+print("File copied successfully.")'''
+
+
+
 
 
 
